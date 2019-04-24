51cc7250c8081e08547a99e16495a270f1324631
Kareem Daggash <kareemdaggash@gmail.com>
2019-04-24 11:45:38 +0100

    style: minor code commenting

--- src/app.js
+++ src/app.js
@@ -1,16 +1,19 @@
+//import all the modules I'll need
 import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import router from './routes/index.js';
-
 const app = express();
 
+//parse incoming requests data
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+//include routes and enable cross origin
 app.use(cors());
 app.use('/api/v1',router);
 
+//listen on port 5000
 const PORT = 5000;
 app.listen(PORT, () => {
   console.log(`server running on port ${PORT}`)
